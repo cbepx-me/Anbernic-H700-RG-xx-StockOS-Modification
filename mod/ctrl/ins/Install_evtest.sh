87f8a1a06727c6a8c85781efa848509e1f72ac2d
cbepx <62635246+cbepx-me@users.noreply.github.com>
2026-01-25 18:48:35 +0800

    Delete mod directory

--- mod/ctrl/ins/Install_evtest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-progdir="$(cd $(dirname "$0"); pwd)"
-
-cp -f "$progdir"/evtest_ins/evtest /mnt/vendor/bin/evtest && chmod 777 /mnt/vendor/bin/evtest
-cp -f "$progdir"/evtest_ins/evtest /usr/bin/evtest && chmod 777 /usr/bin/evtest
-sync
-exit 0
--- mod/ctrl/ins/Install_gdisk.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-progdir="$(cd $(dirname "$0"); pwd)"
-[ -f /usr/sbin/gdisk ] && exit 1
-dpkg -i "$progdir"/gdisk_ins/*.deb
-sync
-exit 0
